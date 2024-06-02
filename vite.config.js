a9b05c82b6274f6fa2740c11a53cc1047951ee83
Raphael Maier <raphaelmaier1997@gmail.com>
2024-06-02 00:27:07 -0400

    adjust vite file

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        imprint: resolve(__dirname, 'imprint.html'),
       },
     },
   },
