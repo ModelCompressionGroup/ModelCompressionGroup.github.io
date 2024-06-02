fa574fbd09324ef0c4c36e3f2ced91ecb508ae10
Raphael Maier <raphaelmaier1997@gmail.com>
2024-06-02 00:01:49 -0400

    reorder scriptes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '', // No subdirectory since this is a GitHub Pages user/organization site
   build: {
     outDir: 'dist',
     rollupOptions: {
