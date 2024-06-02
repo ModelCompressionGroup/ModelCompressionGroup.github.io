387a0d4add21a02d818ac07d9ed60cc4ace01885
Raphael Maier <raphaelmaier1997@gmail.com>
2024-06-01 23:05:57 -0400

    Build and deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,37 +3,36 @@ import themer from "@tailus/themer";
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    content: ["./*.html", "./*.js"],
-    darkMode: "media",
-    safelist: ["isToggled"],
-    theme: {
-      fontFamily: {
-        sans: ['Geist', 'Inter', ...defaultTheme.fontFamily.sans],
-        mono : ['GeistMono', 'fira-code', ...defaultTheme.fontFamily.mono],
-      },
-        keyframes: {
-            loop: {
-                to: {
-                    "offset-distance": "100%",
-                },
-            },
-        },
-        
+  content: ["./*.html", "./*.js"],
+  darkMode: "media",
+  safelist: ["isToggled"],
+  theme: {
+    fontFamily: {
+      sans: ['Geist', 'Inter', ...defaultTheme.fontFamily.sans],
+      mono: ['GeistMono', 'fira-code', ...defaultTheme.fontFamily.mono],
     },
-    plugins: [
-        themer({
-            palette: {
-                extend : "nature"
-            },
-            radius: "smoothest",
-            background: "light",
-            border: "light",
-            padding:"large",
-            components: {
-                button: {
-                    rounded : "2xl"
-                }
-            }
-        })
-    ],
+    keyframes: {
+      loop: {
+        to: {
+          "offset-distance": "100%",
+        },
+      },
+    },
+  },
+  plugins: [
+    themer({
+      palette: {
+        extend: "nature"
+      },
+      radius: "smoothest",
+      background: "light",
+      border: "light",
+      padding: "large",
+      components: {
+        button: {
+          rounded: "2xl"
+        }
+      }
+    })
+  ],
 };
