85d01a9e0d920d34d15cc0e0b8bf538dc1c68c4a
Raphael Maier <raphaelmaier1997@gmail.com>
2024-06-30 23:09:56 -0400

    add imprint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,12 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
-import { Inter } from "next/font/google";
+import { Prata } from "next/font/google";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 import Script from 'next/script'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Prata({ subsets: ["latin"], weight: "400" });
 
 export default function RootLayout({
   children,
