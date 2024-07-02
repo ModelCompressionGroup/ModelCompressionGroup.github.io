dc4a344bb304870947f1562d50a5ac8e3a8a9b9e
Raphael Maier <raphaelmaier1997@gmail.com>
2024-07-01 22:22:42 -0400

    fix

--- src/components/Features/SingleFeature.tsx
+++ src/components/Features/SingleFeature.tsx
@@ -1,32 +1,26 @@
-import Image from "next/image";
 import { Feature } from "@/types/feature";
 
-export interface Feature {
-  id: number;
-  icon: string; // Ensure this is a string
-  title: string;
-  paragraph: string;
-}
-
-const SingleFeature = ({ feature }: { feature: Feature }) => {
-  const { icon, title, paragraph } = feature;
-  return (
-    <div className="w-full">
-      <div className="wow fadeInUp" data-wow-delay=".15s">
-        <div className="mb-10 flex h-[70px] w-[70px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
-          <Image src={icon} alt={title} width={40} height={40} />
-        </div>
-        <div className="flex flex-col justify-between h-full">
-          <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-            {title}
-          </h3>
-          <p className="text-base font-medium leading-relaxed text-body-color text-justify">
-            {paragraph}
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default SingleFeature;
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icons/hand.png",
+    title: "Excessive Time Spent on Manual Tasks",
+    paragraph:
+      "AI engineering teams spend over 50% of their time on tasks like chip selection, model selection, and optimization, slowing down development and limiting innovation.",
+  },
+  {
+    id: 1,
+    icon: "/images/icons/calendar.png",
+    title: "Unpredictable AI Model Optimization Results",
+    paragraph:
+      "Model optimization for edge devices often yields unpredictable results, extending development cycles and increasing resource usage, affecting project timelines.",
+  },
+  {
+    id: 1,
+    icon: "/images/icons/constraint.png",
+    title: "Hardware Constraints on Edge Devices",
+    paragraph:
+      "Integrating AI into existing systems is challenging due to hardware limitations in computation, latency, and memory, leading to substantial integration and operational hurdles.",
+  },
+];
+export default featuresData;
