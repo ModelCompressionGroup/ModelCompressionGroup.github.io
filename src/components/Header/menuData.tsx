84c09eef00061dfd9ea118761743efb44f727d1f
Raphael Maier <raphaelmaier1997@gmail.com>
2024-07-06 17:55:36 -0400

    update navbar

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -3,34 +3,34 @@ import { Menu } from "@/types/menu";
 const menuData: Menu[] = [
   {
     id: 1,
-    title: "Home",
-    path: "/",
-    newTab: false,
-  },
-  {
-    id: 33,
-    title: "Case Studies",
+    title: "Solutions",
     path: "/blog",
     newTab: false,
   },
   {
-    id: 34,
-    title: "TinyML Hub",
+    id: 2,
+    title: "Product",
     path: "https://tinymlhub.com",
     newTab: true,
   },
   {
-    id: 4,
+    id: 3,
     title: "More",
     newTab: false,
     submenu: [
       {
-        id: 41,
+        id: 31,
         title: "About",
         path: "",
         newTab: false
       },
     ],
+  },
+  {
+    id: 4,
+    title: "Login",
+    path: "https://tinymlhub.com",
+    newTab: true,
   },
 ];
 export default menuData;
