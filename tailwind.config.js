83d131f2aa98d842d3d78d11aa233afc9bff2de8
Raphael Maier <69917279+rjmaier7@users.noreply.github.com>
2024-07-15 15:00:35 +0200

    Update tailwind.config.js
    Update the edge spacing.

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,10 @@ module.exports = {
       center: true,
       padding: {
         DEFAULT: "1rem", 
-        sm: "1rem",      
-        md: "1rem",      
-        lg: "1rem",      
-        xl: "10rem",  
+        sm: "2rem",      
+        md: "3rem",      
+        lg: "4rem",      
+        xl: "5rem",  
       },
     },
     screens: {
