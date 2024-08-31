928aab23d69934a3e897c89c0b0736fa01501807
Raphael Maier <raphaelmaier1997@gmail.com>
2024-08-31 11:29:15 -0700

    revert manufacturing roi section

--- src/components/About/AboutSectionRoi.tsx
+++ src/components/About/AboutSectionRoi.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import SectionTitle from "../Common/SectionTitle";
 import HorizontalLine from "../Common/HorizontalLine";
 import NewTag from "../Common/NewTag";
-import ScheduleDemoButton from "../Common/ScheduleDemoButton";
 
 const AboutSectionRoi = () => {
   return (
@@ -10,22 +9,22 @@ const AboutSectionRoi = () => {
       <div className="container">
         <div className="mb-4">
           <p className="text-left text-base font-medium text-body-color sm:text-medium">
-            Identify Risks Locally with Small Language Models
+            Return on Investment in Manufacturing
           </p>
         </div>
         <div className="mb-8">
-          <HorizontalLine />
+          <HorizontalLine/>
         </div>
         <div className="border-b border-body-color/[.15] pb-16 dark:border-white/[.15] md:pb-20 lg:pb-28">
           <div className="-mx-4 flex flex-wrap items-start">
             <div className="w-full px-4 lg:w-1/2">
               <SectionTitle
-                title="Enhance Risk Identification"
-                paragraph="AmpleAI’s Small Language Models (SLMs) empower payment providers to identify and mitigate risks locally. Our cutting-edge models deliver real-time analysis, ensuring secure and efficient transaction processing."
+                title="Maximize Your ROI" 
+                paragraph="You’ll save time and money with AmpleAI. Don’t take it from us; take it from Manufacturers Alliance Foundation. Their study showed 110% return on investment over three years."
                 mb="44px"
               />
               <div className="mt-8 flex justify-center lg:justify-start">
-                <ScheduleDemoButton />
+                <NewTag text="Read the full report" href="https://www.manufacturersalliance.org/sites/default/files/2024-06/AI-CS24-Report-F_0.pdf"/>
               </div>
             </div>
 
@@ -45,17 +44,17 @@ const AboutSectionRoi = () => {
                     />
                   </div>
                   <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                    Real-Time Risk Identification
+                    Revenue Growth
                   </h3>
                   <p className="text-base font-medium leading-relaxed text-body-color sm:text-medium sm:leading-relaxed">
-                    AmpleAI’s SLMs analyze transactions instantly, detecting and flagging suspicious activities to prevent fraud before it impacts your business.
+                    AmpleAI boosts revenue by enhancing production efficiency, reducing costs, and speeding up time-to-market for new products.
                   </p>
                 </div>
                 <div className="mb-9">
                   <div className="w-8 h-8 flex justify-center items-center rotate-3 mb-6 relative">
                     <Image
                       src="/images/icons/uptime.webp"
-                      alt="Operational Efficiency Icon"
+                      alt="Revenue Growth Icon"
                       fill
                       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                       style={{ objectFit: 'contain' }}
@@ -63,17 +62,17 @@ const AboutSectionRoi = () => {
                     />
                   </div>
                   <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                    Operational Efficiency
+                    Improved Uptime
                   </h3>
                   <p className="text-base font-medium leading-relaxed text-body-color sm:text-medium sm:leading-relaxed">
-                    Our optimized SLMs ensure rapid processing of high-volume transactions, minimizing delays and maintaining seamless operations without compromising security.
+                    Tiny AI models from AmpleAI monitor manufacturing lines, reliably predicting downtime and improving operational uptime.
                   </p>
                 </div>
                 <div className="mb-1">
                   <div className="w-8 h-8 flex justify-center items-center rotate-3 mb-6 relative">
                     <Image
                       src="/images/icons/quality-control.webp"
-                      alt="Scalability Icon"
+                      alt="Revenue Growth Icon"
                       fill
                       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                       style={{ objectFit: 'contain' }}
@@ -81,10 +80,10 @@ const AboutSectionRoi = () => {
                     />
                   </div>
                   <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                    Scalability and Flexibility
+                    Product Quality
                   </h3>
                   <p className="text-base font-medium leading-relaxed text-body-color sm:text-medium sm:leading-relaxed">
-                    AmpleAI’s scalable SLMs adapt to your growing transaction volumes, ensuring consistent performance and robust risk management as your business expands.
+                    AmpleAI ensures competitive and innovative products, enhancing quality and driving long-term profitability.
                   </p>
                 </div>
               </div>
